75f6da65268be427b6bfe79ece20e7fb5c9035f3
jai-mern <Kjaya8862@gamail.com>
2023-11-18 18:16:27 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,64 @@
 import React, { useState } from 'react';
-import ProductList from './ProductList'; // Correct the path
-import Cart from './Cart'; // Correct the path
 
-const App = () => {
-  const [cartItems, setCartItems] = useState([]);
-
-  const products = [
-    { id: 1, name: 'Product 1', description: 'Description 1' },
-    { id: 2, name: 'Product 2', description: 'Description 2' },
+const ShoppingCart = () => {
+  const [products, setProducts] = useState([
+    { id: 1, name: 'Product 1', description: 'Description for Product 1', inCart: false },
+    { id: 2, name: 'Product 2', description: 'Description for Product 2', inCart: false },
     // Add more products as needed
-  ];
+  ]);
+
+  const [cart, setCart] = useState([]);
 
   const addToCart = (productId) => {
-    setCartItems((prevCartItems) => [...prevCartItems, productId]);
+    const updatedProducts = products.map((product) =>
+      product.id === productId ? { ...product, inCart: true } : product
+    );
+    setProducts(updatedProducts);
+
+    const selectedProduct = products.find((product) => product.id === productId);
+    setCart([...cart, selectedProduct]);
   };
 
   const removeFromCart = (productId) => {
-    setCartItems((prevCartItems) => prevCartItems.filter((id) => id !== productId));
+    const updatedProducts = products.map((product) =>
+      product.id === productId ? { ...product, inCart: false } : product
+    );
+    setProducts(updatedProducts);
+
+    const updatedCart = cart.filter((product) => product.id !== productId);
+    setCart(updatedCart);
   };
 
   return (
     <div>
-      <ProductList
-        products={products}
-        addToCart={addToCart}
-        removeFromCart={removeFromCart}
-        cartItems={cartItems}
-      />
-      <Cart cartItems={cartItems} removeFromCart={removeFromCart} />
+      <h2>Available Products</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <strong>{product.name}</strong>
+            <p>{product.description}</p>
+            {product.inCart ? (
+              <button onClick={() => removeFromCart(product.id)}>Remove from Cart</button>
+            ) : (
+              <button onClick={() => addToCart(product.id)}>Add to Cart</button>
+            )}
+          </li>
+        ))}
+      </ul>
+
+      <h2>Shopping Cart</h2>
+      <ul>
+        {cart.map((product) => (
+          <li key={product.id}>
+            <strong>{product.name}</strong>
+            <p>{product.description}</p>
+          </li>
+        ))}
+      </ul>
+
+      <p>Total Items in Cart: {cart.length}</p>
     </div>
   );
 };
 
-export default App;
+export default ShoppingCart;
